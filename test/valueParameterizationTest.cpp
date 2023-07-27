8a02d5a4f0d7fb0c23702f0031e60e0e3740ffa9
Venugopal  K (C) <vgk@tritechsamvit.com>
2023-07-27 12:05:56 +0530

    Paremeterized Test

---
+++ test/valueParameterizationTest.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+int divFun(int numerator,int denominator){
+    if(denominator==0 || denominator < 0){return 0;}
+
+    return numerator/denominator;
+}
+TEST(DivFunTestSuite,HandleValidInputs){
+    int numerator=10;
+    int denomiator=5;
+    int expectedValue=2;
+    int actualValue=divFun(numerator,denomiator);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST(DivFunTestSuite,HandleDivideByZero){
+    int numerator=10;
+    int denomiator=0;
+    int expectedValue=0;
+    int actualValue=divFun(numerator,denomiator);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST(DivFunTestSuite,HandleNegetiveDenominator){
+    int numerator=10;
+    int denomiator=-5;
+    int expectedValue=0;
+    int actualValue=divFun(numerator,denomiator);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+
+
